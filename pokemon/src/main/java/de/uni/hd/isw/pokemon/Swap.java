3ac00dfc5dff41bb069a488844681271aca42f77
Igor Dimitrov <igor-dimi@protonmail.com>
2024-10-24 17:33:56 +0200

    add Swap class Constructor implementaiton

--- pokemon/src/main/java/de/uni/hd/isw/pokemon/Swap.java
+++ pokemon/src/main/java/de/uni/hd/isw/pokemon/Swap.java
@@ -2,5 +2,22 @@ package de.uni.hd.isw.pokemon;
 import java.util.Date;
 
 public class Swap {
+    private Trainer trainer1;
+    private Trainer trainer2;
+    private Pokemon pokemon1;
+    private Pokemon pokemon2;
+    private String swapID;
+    private static long nextSwapID = 1;
+    public Swap(Trainer trainer1,
+                Trainer trainer2,
+                Pokemon pokemon1,
+                Pokemon pokemon2) {
+        this.trainer1 = trainer1;
+        this.trainer2 = trainer2;
+        this.pokemon1 = pokemon1;
+        this.pokemon2 = pokemon2;
+        swapID = String.valueOf(nextSwapID);
+        nextSwapID++;
+    }
 
 }
